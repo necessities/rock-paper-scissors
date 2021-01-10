be68b7ffbfa29b791a06d02b5092ba49d449e5e3
necessities <bear_necessities@icloud.com>
2021-01-09 23:08:57 -0500

    commit 3
    updated file names + moved js to its own file

--- app.js
+++ app.js
@@ -1,7 +1,68 @@
 
-const button = document.querySelector('button');
 
-button.onclick = function() {
-  let name = prompt('What is your name?');
-  alert('Hello ' + name + ', nice to see you!');
+
+
+
+// function for generating computer input
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+
+//prompt for user input
+
+let choice = prompt('Rock Paper or Scissors?', );
+
+//declare variable to hold computer input
+
+let computerChoice = "change";
+
+//convert computer input into either rock,paper,scissors
+
+function getComputerChoice() {
+  computerChoice = (getRandomInt(3));
+  //console.log `${computerChoice}`; : Test output to see if function works
+}
+  getComputerChoice();
+
+// alert(computerChoice); : Test output to see if function works with this
+
+if (computerChoice == 0) {
+  computerChoice = 'rock';
+} else if (computerChoice == 1) {
+  computerChoice = 'paper'
+} else if (computerChoice == 2) {
+  computerChoice = 'scissors'
+}
+
+alert(`Computer chooses ${computerChoice}`);
+
+//take user input and check it against computer input
+
+if (choice == 'rock') {
+  if(choice == 'rock' && computerChoice == 'rock'){
+  alert("Tie!");
+  } else if (choice == 'rock' && computerChoice == 'paper') {
+  alert("Computer wins!");
+  } else if (choice == 'rock'&& computerChoice == 'scissors') {
+  alert("You win!");}
+
+} else if (choice == 'paper') {
+  if(choice == 'paper' && computerChoice == 'paper'){
+  alert("Tie!");
+  } else if (choice == 'paper' && computerChoice == 'scissors') {
+  alert("Computer wins!");
+  } else if (choice == 'paper'&& computerChoice == 'rock') {
+  alert("You win!");}
+
+} else if (choice == 'scissors') {
+  if(choice == 'scissors' && computerChoice == 'scissors'){
+  alert("Tie!");
+  } else if (choice == 'scissors' && computerChoice == 'rock') {
+  alert("Computer wins!");
+} else if (choice == 'scissors'&& computerChoice == 'paper') {
+  alert("You win!");}
+
+
+} else {
+  alert("Please Choose rock paper or scissors")
 }
